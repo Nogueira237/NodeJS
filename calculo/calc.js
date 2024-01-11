a6c169d7d4ef4e1cf46fc75f4c387ce122690ba3
Eduardo Nogueira <edunogueira06@hotmail.com>
2024-01-11 15:18:20 -0300

    Aula 4 node

---
+++ calculo/calc.js
@@ -0,0 +1,8 @@
+var SomaFunc = require("./funcoes/soma");
+var SubFunc = require("./funcoes/sub");
+var MultiFunc = require("./funcoes/multi");
+var DivFunc = require("./funcoes/div");
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(10,5));
+console.log(MultiFunc(100,10));
