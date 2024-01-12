45051b82c239b4848347a9912b9f0c1308ba9a4a
Eduardo Nogueira <edunogueira06@hotmail.com>
2024-01-12 16:05:34 -0300

    Aula 10 node

--- Test1/index.js
+++ Test1/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Bem-vindos ao meu app!")
+    res.sendFile(__dirname + "/html/index.html");
 });
 
 app.get("/sobre", function(req, res){
