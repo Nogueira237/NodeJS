3dbc4b19e09fb8d319d57d1d6e6e0fd8034b2b31
Eduardo Nogueira <edunogueira06@hotmail.com>
2024-01-12 15:54:14 -0300

    Aula 8 node

--- Test1/index.js
+++ Test1/index.js
@@ -13,6 +13,10 @@ app.get("/blog", function(req, res){
     res.send("Bem-vindo ao meu blog")
 })
 
+app.get("/ola/:nome/:cargo", function(req, res){
+    res.send("Ola " + req.params.nome + " Seu cargo e: " + req.params.cargo);
+})
+
 app.listen(8081, function(){
     console.log("Servidor rodando na url https://localhost:8081")
 })
